2bf2bb149f9dd4b06c52ac738d957f3e848c0cad
Hanse44 <hanse44@wwu.edu>
2025-07-14 14:09:55 -0700

    Board added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import touchPic from './assets/touch.jpg'
-import viteLogo from '/vite.svg'
+import test1 from './assets/test1.png'
 import './App.css'
 
 function App() {
@@ -10,18 +9,13 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 style={{ fontSize: "20px" }}>Hello, world!</h1>
+        <img src={touchPic} alt="Logo" width="400" />
       </div>
-      <div>
-        <h1>Hello, world!</h1>
-        <img src={logo} alt="Logo" width="200" />
+            <div>
+        <h1>Development Board</h1>
+        <img src={test1} alt="Logo" width="600" />
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,9 +24,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
